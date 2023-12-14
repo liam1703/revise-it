bafb9b32f52aaf598a874eff84e31b9cc72bdb0a
Liam Bradbury <liam.bradbury@nurole.com>
2023-12-14 21:03:10 +0000

    Change project relative path

--- backend/go.mod
+++ backend/go.mod
@@ -1,4 +1,4 @@
-module Users/liambradbury/Learning/2024/revise-it/backend
+module revise-it/backend
 
 go 1.18
 
--- backend/main.go
+++ backend/main.go
@@ -4,6 +4,8 @@ package main
 import (
 	"net/http"
 
+	"revise-it/backend/configs"
+
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,12 +13,16 @@ func main() {
 	// Create a new Gin router
 	router := gin.Default()
 
-	// Define your routes and handlers here
+	// Connect to database
+	configs.ConnectDB()
+
+	//Define your routes and handlers here
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
 			"message": "Hello, Flashcard App!",
 		})
 	})
+
 	// Start the server
 	router.Run(":8080")
 }
