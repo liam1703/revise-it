a9d5bfe6bc704d47147ae491cec52546d6bb1cc2
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-03 13:24:34 +0000

    Add user model

---
+++ backend/models/user_model.go
@@ -0,0 +1,9 @@
+package models
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type User struct {
+	ID       primitive.ObjectID `bson:"_id"`
+	Username *string            `json:"username"`
+	Password *string            `json:"password"`
+}
