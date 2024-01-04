796a63f71d3339ea10a06aa5a7ea1468c3685266
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-04 18:28:03 +0000

    Add password hashing and checking

--- backend/controllers/user_controller.go
+++ backend/controllers/user_controller.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/go-playground/validator/v10"
+	"golang.org/x/crypto/bcrypt"
 
 	"revise-it/backend/database"
 
@@ -10,3 +14,27 @@ import (
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 var validate = validator.New()
+
+//HashPassword is used to encrypt the password before it is stored in the DB
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	return string(bytes)
+}
+
+//VerifyPassword checks the input password while verifying it with the passward in the DB.
+func VerifyPassword(userPassword string, providedPassword string) (bool, string) {
+	err := bcrypt.CompareHashAndPassword([]byte(providedPassword), []byte(userPassword))
+	check := true
+	msg := ""
+
+	if err != nil {
+		msg = fmt.Sprintf("login or passowrd is incorrect")
+		check = false
+	}
+
+	return check, msg
+}
