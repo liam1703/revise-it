0f26f9ebd5691a198e688601a05277837c525533
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-14 18:09:22 +0000

    Tidy up view

--- frontend/src/views/CreateFlashcard.vue
+++ frontend/src/views/CreateFlashcard.vue
@@ -1,15 +1,18 @@
 <template>
 <div class="mx-auto w-1/2 mt-24">
-  <label for="question" class="text-sm font-medium">Your question</label>
+  <label for="question" class="text-sm font-medium">Your question:</label>
   <textarea id="question" rows="6" class="mt-5 p-1 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
   placeholder="Write your question here...">
   </textarea>
 </div>
 <div class="mx-auto w-1/2 mt-16">
-  <label for="answer" class="text-sm font-medium">Your Answer</label>
+  <label for="answer" class="text-sm font-medium">Your Answer:</label>
   <textarea id="answer" rows="6" class="mt-5 p-1 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
   placeholder="Write your answer here...">
   </textarea>
+</div>
+<div class="mt-12 w-1/2 mx-auto">
+  <button class="border border-solid p-3 rounded bg-red-300">Create card</button>
 </div>
 
 
@@ -23,6 +26,3 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
