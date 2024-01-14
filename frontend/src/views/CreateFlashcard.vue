e7c4f02d694f0071602db95f71739892f63ecc31
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-14 17:59:03 +0000

    Seperate text box for question and answer

--- frontend/src/views/CreateFlashcard.vue
+++ frontend/src/views/CreateFlashcard.vue
@@ -1,8 +1,14 @@
 <template>
 <div class="mx-auto w-1/2 mt-24">
-  <label for="message" class="text-sm font-medium">Your message</label>
-  <textarea id="message" rows="6" class="mt-5 p-1 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+  <label for="question" class="text-sm font-medium">Your question</label>
+  <textarea id="question" rows="6" class="mt-5 p-1 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
   placeholder="Write your question here...">
+  </textarea>
+</div>
+<div class="mx-auto w-1/2 mt-16">
+  <label for="answer" class="text-sm font-medium">Your Answer</label>
+  <textarea id="answer" rows="6" class="mt-5 p-1 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+  placeholder="Write your answer here...">
   </textarea>
 </div>
 
