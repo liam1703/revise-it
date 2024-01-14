b6527c708d345688e59c127662c2660adbd5dafb
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-14 18:42:05 +0000

    Annoying typo

--- frontend/src/views/CreateFlashcard.vue
+++ frontend/src/views/CreateFlashcard.vue
@@ -1,6 +1,6 @@
 <template>
 <div class="mx-auto w-1/2 mt-24">
-  <label for="question" class="text-sm font-medium">Your question:</label>
+  <label for="question" class="text-sm font-medium">Your Question:</label>
   <textarea id="question" rows="6" class="mt-5 p-1 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
   placeholder="Write your question here...">
   </textarea>
