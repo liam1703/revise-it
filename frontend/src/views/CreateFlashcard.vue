3b41e8c964b860c914af0556ecee29fafbdaa554
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-14 17:54:14 +0000

    Example textarea

---
+++ frontend/src/views/CreateFlashcard.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="mx-auto w-1/2 mt-24">
+  <label for="message" class=" mb-2 text-sm font-medium">Your message</label>
+  <textarea id="message" label="message" rows="4" class="block p-1 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here...">
+  </textarea>
+</div>
+
+
+
+
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
