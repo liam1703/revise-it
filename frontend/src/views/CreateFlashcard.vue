27de218bba5ba87d3cad0fa4766dd943bb408a37
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-14 18:53:05 +0000

    Data bind on question

--- frontend/src/views/CreateFlashcard.vue
+++ frontend/src/views/CreateFlashcard.vue
@@ -2,27 +2,31 @@
 <div class="mx-auto w-1/2 mt-24">
   <label for="question" class="text-sm font-medium">Your Question:</label>
   <textarea id="question" rows="6" class="mt-5 p-1 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-  placeholder="Write your question here...">
+  placeholder="Write your question here..."
+  v-model="question"
+  >
   </textarea>
 </div>
 <div class="mx-auto w-1/2 mt-16">
   <label for="answer" class="text-sm font-medium">Your Answer:</label>
   <textarea id="answer" rows="6" class="mt-5 p-1 w-full text-sm  bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
-  placeholder="Write your answer here...">
+  placeholder="Write your answer here..."
+  >
   </textarea>
 </div>
 <div class="mt-12 w-1/2 mx-auto">
-  <button class="border border-solid p-3 rounded bg-red-300">Create card</button>
+  <button class="border border-solid p-3 rounded bg-red-300" @click="showQuestion">Create card</button>
 </div>
-
-
-
-
 </template>
 
-<script>
-export default {
+<script setup>
+import { ref } from 'vue';
 
+const question = ref('');
+const answer = ref('');
+
+function showQuestion() {
+  console.log(question.value);
 }
 </script>
 
