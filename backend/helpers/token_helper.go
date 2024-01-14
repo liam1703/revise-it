0376a12953cf0ca275520f63ab3a747fd3c983cf
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-14 18:41:34 +0000

    Add more to token helper

--- backend/helpers/token_helper.go
+++ backend/helpers/token_helper.go
@@ -1,6 +1,7 @@
 package helper
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"time"
@@ -10,6 +11,15 @@ import (
 	jwt "github.com/dgrijalva/jwt-go"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+// SignedDetails
+type SignedDetails struct {
+	Email      string
+	First_name string
+	Last_name  string
+	Uid        string
+	jwt.StandardClaims
+}
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "user")
 
@@ -44,11 +54,33 @@ func GenerateAllTokens(email string, firstName string, lastName string, uid stri
 	return token, refreshToken, err
 }
 
-// SignedDetails
-type SignedDetails struct {
-	Email      string
-	First_name string
-	Last_name  string
-	Uid        string
-	jwt.StandardClaims
+//ValidateToken validates the jwt token
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(SECRET_KEY), nil
+		},
+	)
+
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = fmt.Sprintf("the token is invalid")
+		msg = err.Error()
+		return
+	}
+
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = fmt.Sprintf("token is expired")
+		msg = err.Error()
+		return
+	}
+
+	return claims, msg
 }
