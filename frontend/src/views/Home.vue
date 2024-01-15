c2b6ecb5ca78c65b40318bb97133e4f6f9901e08
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-15 10:43:54 +0000

    Setup login form

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,8 @@
 <template>
-  <h1 class="mb-12">Homepage</h1>
-  <router-link to="/login" class="m-4 border border-solid p-3 rounded bg-red-300"><button>Login</button></router-link>
+  <div class="bg-slate-200 h-screen w-screen">
+    <h1 class="mb-12">Homepage</h1>
+    <router-link to="/login" class="m-4 border border-solid p-3 rounded bg-red-300"><button>Login</button></router-link>
+  </div>
 </template>
 
 <script setup>
--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -1,5 +1,14 @@
 <template>
-  <h2>LOGIN PAGE</h2>
+  <div class="h-screen w-screen bg-slate-100">
+    <div class="w-full  pt-24">
+      <h1 class="text-4xl text-center">Revise-it</h1>
+      <div class="bg-white shadow-xl w-96 p-4 rounded-xl mx-auto mt-16">
+        <h3 class="mb-3">Log into your Revise-it account.</h3>
+        <label for="email" class="text-sm font-medium">Email:</label>
+        <input id="email" type="text" class="mt-2 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
