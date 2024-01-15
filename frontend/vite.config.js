7e19e552d95896c8481d60ec982957687151571d
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-15 11:20:37 +0000

    Alter vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { loadEnv, defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
-})
+export default ({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  // expose .env as process.env instead of import.meta since jest does not not support import meta yet
+  const processEnvValues = {
+    "process.env": Object.entries(env).reduce(
+      (prev, [key, val]) => ({
+        ...prev,
+        [key]: val,
+      }),
+      {}
+    ),
+  };
+
+  return defineConfig({
+    plugins: [
+      vue(),
+    ],
+    define: processEnvValues,
+  });
+};
