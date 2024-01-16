5fceda2265a399e3e491b01907170cda7327ffde
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-16 10:44:29 +0000

    Add in signup page

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from "vue-router";
 import CreateFlashcard from "./views/CreateFlashcard.vue";
 import Home from "./views/Home.vue"
 import Login from "./views/Login.vue"
+import SignUp from "./views/SignUp.vue"
+
 
 
 
@@ -15,6 +17,10 @@ export default createRouter({
     {
       path: "/login",
       component: Login
+    },
+    {
+      path: "/sign-up",
+      component: SignUp
     },
     {
       path:"/create-new-flashcard",
