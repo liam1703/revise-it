04a7dc88bd94e4ed5b511c4b87eeace0abdbd16a
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-17 15:14:36 +0000

    Comment out some defaults

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-axios.defaults.withCredentials = true;
-axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.baseURL = "http://127.0.0.1:8000";
+// axios.defaults.withCredentials = true;
+// axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
+// axios.defaults.baseURL = "http://127.0.0.1:8000";
 
 const apiAxios = axios.create({
   baseURL: "http://127.0.0.1:8000",
--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './index.css'
 import router from "./router";
 import App from './App.vue'
+import "./lib/axios";
 
 // Styles
 import "./assets/styles.css";
