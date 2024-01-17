df66ae427900f109c0a12dd986151c023a2f39ca
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-17 13:14:22 +0000

    Add cors

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 axios.defaults.withCredentials = true;
 axios.defaults.headers["X-Requested-With"] = "XMLHttpRequest";
-axios.defaults.baseURL = "http://localhost:8000";
+axios.defaults.baseURL = "http://127.0.0.1:8000";
 
 const apiAxios = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "http://127.0.0.1:8000",
 });
 
 export { apiAxios };
