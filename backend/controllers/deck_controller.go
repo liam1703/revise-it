10bd9d6922c0da23a904352016b6a26461360c90
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-17 12:56:42 +0000

    Update some styles and check go login function works

--- backend/controllers/deck_controller.go
+++ backend/controllers/deck_controller.go
@@ -1,20 +1,20 @@
 package controllers
 
-import (
-	"context"
-	"revise-it/backend/models"
-	"time"
+// import (
+// 	"context"
+// 	"revise-it/backend/models"
+// 	"time"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// )
 
-func addDeck() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-		// add deck to db with ref to user
-		var deck models.Deck
-	}
-}
+// func addDeck() gin.HandlerFunc {
+// 	return func(c *gin.Context) {
+// 		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+// 		// add deck to db with ref to user
+// 		var deck models.Deck
+// 	}
+// }
 
 // func for deleting deck
 
--- frontend/src/components/navbar.vue
+++ frontend/src/components/navbar.vue
@@ -1,5 +1,6 @@
 <template>
-  <div class="w-full h-24 bg-blue-300 flex">
+  <div class="w-full h-24 background-base text-white flex space-x-28 pl-32 items-center">
+    <h1 class="">Revise-it</h1>
     <router-link to="/"><p>Home</p></router-link>
     <router-link to="/login"><p>Login</p></router-link>
   </div>
