40b43cf911e399e5cbe7ead59d5ca3cdecd57941
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-19 18:46:31 +0000

    Dummy signup form

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -1,11 +1,39 @@
 <template>
-  <div>
-    <h3>Signup page</h3>
+  <div class="h-screen w-screen bg-slate-100">
+    <div class="w-full  pt-24">
+      <h1 class="text-4xl text-center">Revise-it</h1>
+      <div class="bg-white shadow-xl w-96 p-4 rounded-xl mx-auto mt-16">
+        <h3 class="mb-3">Log into your Revise-it account.</h3>
+        <form>
+          <label for="email" class="text-sm font-medium">Email:</label>
+          <input v-model="email" id="email" type="text" class="mt-2 mb-4 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+          <label for="password" class="text-sm font-medium">Password:</label>
+          <input v-model="password" id="password" type="password" class="mt-2 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+
+        <div class="my-2">
+          <router-link to="/login"><p>Have an account already? Click here...</p></router-link>
+        </div>
+        <div class="mt-12 text-center">
+          <button class="border border-solid p-3 rounded background-base text-white" @click="handleSignup">Signup</button>
+        </div>
+      </form>
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-export default {
+<script setup>
+import { ref } from "vue";
+import { apiAxios } from "../lib/axios";
 
+const email = ref('');
+const password = ref('');
+
+async function handleSignup() {
+  // const { data } = await apiAxios.post(`/users/login`, {
+  //   email: email.value,
+  //   password: password.value,
+  // });
+  console.log("Sign up!!!");
 }
 </script>
