c4bbc3176e13ae7741350b8447e702ef41c2a60b
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-19 18:45:08 +0000

    Login form

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -4,16 +4,18 @@
       <h1 class="text-4xl text-center">Revise-it</h1>
       <div class="bg-white shadow-xl w-96 p-4 rounded-xl mx-auto mt-16">
         <h3 class="mb-3">Log into your Revise-it account.</h3>
-        <label for="email" class="text-sm font-medium">Email:</label>
-        <input v-model="email" id="email" type="text" class="mt-2 mb-4 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
-        <label for="password" class="text-sm font-medium">Password:</label>
-        <input v-model="password" id="password" type="password" class="mt-2 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
-        <div class="my-2">
-          <router-link to="/sign-up"><p>Dont have an account sign up here?</p></router-link>
-        </div>
-        <div class="mt-12 text-center">
-          <button class="border border-solid p-3 rounded background-base text-white" @click="handleLogin">Login</button>
-        </div>
+        <form>
+          <label for="email" class="text-sm font-medium">Email:</label>
+          <input v-model="email" id="email" type="text" class="mt-2 mb-4 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+          <label for="password" class="text-sm font-medium">Password:</label>
+          <input v-model="password" id="password" type="password" class="mt-2 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+          <div class="my-2">
+            <router-link to="/sign-up"><p>Dont have an account sign up here?</p></router-link>
+          </div>
+          <div class="mt-12 text-center">
+            <button class="border border-solid p-3 rounded background-base text-white" @click="handleLogin">Login</button>
+          </div>
+        </form>
       </div>
     </div>
   </div>
