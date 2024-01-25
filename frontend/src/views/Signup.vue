de558eda73837b549f9c2f18f3e028e1a8c0da74
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-25 19:37:39 +0000

    Create readme

--- frontend/src/views/Signup.vue
+++ frontend/src/views/Signup.vue
@@ -3,7 +3,7 @@
     <div class="w-full  pt-24">
       <router-link to="/" class="cursor-pointer"><h1 class="text-4xl text-center">Revise-it</h1></router-link>
       <div class="bg-white shadow-xl w-96 p-4 rounded-xl mx-auto mt-16">
-        <h3 class="mb-3">Log into your Revise-it account.</h3>
+        <h3 class="mb-3">Register for a Revise-it account.</h3>
         <form>
           <label for="email" class="text-sm font-medium">Email:</label>
           <input v-model="email" id="email" type="text" class="mt-2 mb-4 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
