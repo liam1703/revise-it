52ea9a586a2e88192e64fb48d37ce3968535651e
Liam Bradbury <liam.bradbury@nurole.com>
2024-01-27 10:08:04 +0000

    Import create deck card

--- frontend/src/views/MyDecks.vue
+++ frontend/src/views/MyDecks.vue
@@ -1,8 +1,12 @@
 <template>
   <navbar />
   <h1 class="text-4xl text-center mt-12">My Decks</h1>
+  <create-deck-card />
+
 </template>
 
 <script setup>
 import Navbar from '../components/Navbar.vue';
+import CreateDeckCard from '../components/CreateDeckCard.vue';
+// createdeckcard is a bad name?
 </script>
