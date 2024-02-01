2dd5f9d10e7753bb7cbed6665e110ec02a5acb5e
Liam Bradbury <liam.bradbury@nurole.com>
2024-02-01 20:26:42 +0000

    Finish homepage starter

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="bg-slate-200 h-screen w-screen">
     <navbar />
-    <div>
-      <h1 class="text-center text-6xl mt-16 font-semibold">Revision backed by science.</h1>
-      <h4 class="text-center text-xl mt-8">Create questions and learn using active recall.</h4>
+    <div class="w-screen">
+      <h1 class="text-center text-6xl mt-16 font-semibold text-indigo-600">Revision backed by science.</h1>
+      <h4 class="text-center text-xl mt-8 text-black">Create questions and learn using active recall.</h4>
       <div class="text-center w-screen mt-6">
         <button @click.prevent="goToLogin" class="text-center border border-solid bg-purple-600 text-white p-3 hover:rounded-lg">Begin learning</button>
       </div>
-
+      <div class="bg-gray-300 text-center rounded-lg w-10/12 mx-auto mt-24 p-8">
+        <h5 class="text-indigo-600 font-bold text-xl">What do we do?</h5>
+      </div>
     </div>
   </div>
 </template>
