3dc7f9fa3dd72fddabfe22ed3be191e8ae3287c2
Liam Bradbury <liam.bradbury@nurole.com>
2024-02-01 19:25:50 +0000

    Work on homepage

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="bg-slate-200 h-screen w-screen">
     <navbar />
-    <h1 class="mb-12">Homepage</h1>
-    <router-link to="/login" class="m-4 border border-solid p-3 rounded bg-red-300"><button>Login</button></router-link>
+    <div>
+      <h1 class="text-center text-6xl mt-16 font-semibold">Revision backed by science.</h1>
+      <h4 class="text-center text-xl mt-8">Create questions and learn using active recall.</h4>
+      <div class="mx-auto">
+        <button class="text-center">Begin learning</button>
+      </div>
+
+    </div>
   </div>
 </template>
 
