e9bc53ca1a2205c658684ecc6eb5791f7cdd5241
Liam Bradbury <liam.bradbury@nurole.com>
2024-02-02 18:11:12 +0000

    Point to sign up not login

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -5,7 +5,7 @@
       <h1 class="text-center text-6xl mt-16 font-semibold text-indigo-600">Revision backed by science.</h1>
       <h4 class="text-center text-xl mt-8 text-black">Create questions and learn using active recall.</h4>
       <div class="text-center w-screen mt-6">
-        <button @click.prevent="goToLogin" class="text-center border border-solid bg-purple-600 text-white p-3 hover:rounded-lg">Begin learning</button>
+        <button @click.prevent="goToSignup" class="text-center border border-solid bg-purple-600 text-white p-3 hover:rounded-lg">Begin learning</button>
       </div>
       <div class="bg-gray-300 text-center rounded-lg w-10/12 mx-auto mt-24 p-8">
         <h5 class="text-indigo-600 font-bold text-xl">What do we do?</h5>
@@ -16,7 +16,7 @@
 
 <script setup>
 import Navbar from '../components/Navbar.vue';
-function goToLogin() {
-  window.location.href = '/login';
+function goToSignup() {
+  window.location.href = '/sign-up';
 }
 </script>
