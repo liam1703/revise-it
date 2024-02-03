1cf659ebea58098eeaba5caf70cb436e7083dddb
Liam Bradbury <liam.bradbury@nurole.com>
2024-02-03 08:52:30 +0000

    Set modal condition

--- frontend/src/components/CreateDeckCard.vue
+++ frontend/src/components/CreateDeckCard.vue
@@ -1,11 +1,12 @@
 <template>
   <div>
-    <a href="#" class="block max-w-xs p-6 max-h-sm h-72  border rounded-lg shadow text-center hover:bg-gray-700 bg-gray-800 border-gray-700">
+    <button @click.prevent="createDeckModal = true"
+      class="block max-w-xs p-6 max-h-sm h-72  border rounded-lg shadow text-center hover:bg-gray-700 bg-gray-800 border-gray-700">
       <h5 class="mb-12 text-2xl font-bold tracking-tight text-white ">Create new deck</h5>
       <p class="font-normal text-gray-400">
         Click here to create a new deck of flashcards.
       </p>
-    </a>
+    </button>
   </div>
 
 
@@ -14,4 +15,6 @@
 </template>
 
 <script setup>
+import { ref } from 'vue';
+const createDeckModal = ref(false);
 </script>
