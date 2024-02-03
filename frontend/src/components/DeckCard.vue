46bc18016b865122935feca27597dfd7a746bc1a
Liam Bradbury <liam.bradbury@nurole.com>
2024-02-03 08:25:18 +0000

    Placeholding deck component

---
+++ frontend/src/components/DeckCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <a href="#" class="block max-w-xs p-6 max-h-sm h-72  border rounded-lg shadow text-center hover:bg-gray-700 bg-pink-600 border-black">
+      <h5 class="mb-12 text-2xl font-bold tracking-tight text-white ">Your deck</h5>
+      <p class="font-normal text-gray-400">
+        Heres your deck
+      </p>
+    </a>
+  </div>
+</template>
+
+<script setup>
+</script>
