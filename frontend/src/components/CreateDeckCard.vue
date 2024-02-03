1a7c6b54e49dbf8b0a28a23ec80ad3259421abe7
Liam Bradbury <liam.bradbury@nurole.com>
2024-02-03 06:50:25 +0000

    Fix styling

--- frontend/src/components/CreateDeckCard.vue
+++ frontend/src/components/CreateDeckCard.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a href="#" class="block max-w-xs p-6 max-h-sm h-72  border rounded-lg shadow text-center hover:bg-gray-700">
+    <a href="#" class="block max-w-xs p-6 max-h-sm h-72  border rounded-lg shadow text-center hover:bg-gray-700 bg-gray-800 border-gray-700">
       <h5 class="mb-12 text-2xl font-bold tracking-tight text-white ">Create new deck</h5>
       <p class="font-normal text-gray-400">
         Click here to create a new deck of flashcards.
