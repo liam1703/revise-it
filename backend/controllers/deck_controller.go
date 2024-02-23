fe25fe640df076d6efa5ebc3344b7bfe0cacdb9d
Liam Bradbury <liam.bradbury98@gmail.com>
2024-02-23 08:45:20 +0000

    Start the add deck method

--- backend/controllers/deck_controller.go
+++ backend/controllers/deck_controller.go
@@ -15,6 +15,23 @@ func AddDeck() gin.HandlerFunc {
 		// add deck to db with ref to user
 		var deck models.Deck
 		fmt.Println(ctx, "deck", deck)
+		if err := c.BindJSON(&deck); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+
+		validationErr := validate.Struct(deck)
+		if validationErr != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+			return
+		}
+
+		deck.Created_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		deck.Updated_at, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+		deck.ID = primitive.NewObjectID()
+
+		fmt.Println(ctx, "deck", deck)
+
 		defer cancel()
 	}
 }
