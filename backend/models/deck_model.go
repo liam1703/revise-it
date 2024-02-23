61417726987c411d210f76d0ed31262d0a89a2bc
Liam Bradbury <liam.bradbury98@gmail.com>
2024-02-23 08:45:59 +0000

    Add user id too deck model

--- backend/models/deck_model.go
+++ backend/models/deck_model.go
@@ -5,6 +5,7 @@ type Deck struct {
 	ID         primitive.ObjectID `bson:"_id"`
 	Name       string
 	Description string
+	user_id     string
 	Flashcards []Flashcard
 	Created_at    time.Time          `json:"created_at"`
 	Updated_at    time.Time          `json:"updated_at"`
