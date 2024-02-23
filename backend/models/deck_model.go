b26e1dcc458ca98cb6e5946a91b4c16a45f24bd8
Liam Bradbury <liam.bradbury98@gmail.com>
2024-02-23 08:44:49 +0000

    Update deck model

--- backend/models/deck_model.go
+++ backend/models/deck_model.go
@@ -2,6 +2,10 @@ package models
 
 // deck represents a deck of flashcards.
 type Deck struct {
+	ID         primitive.ObjectID `bson:"_id"`
 	Name       string
+	Description string
 	Flashcards []Flashcard
+	Created_at    time.Time          `json:"created_at"`
+	Updated_at    time.Time          `json:"updated_at"`
 }
