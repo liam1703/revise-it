437b5d284a618444b21e0cea774115f178d24698
Liam Bradbury <liam.bradbury98@gmail.com>
2024-03-15 20:29:20 +0000

    Alter deck model

--- backend/models/deck_model.go
+++ backend/models/deck_model.go
@@ -1,12 +1,18 @@
 package models
 
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 // deck represents a deck of flashcards.
 type Deck struct {
-	ID         primitive.ObjectID `bson:"_id"`
-	Name       string
+	ID          primitive.ObjectID `bson:"_id"`
+	Name        string
 	Description string
 	user_id     string
-	Flashcards []Flashcard
-	Created_at    time.Time          `json:"created_at"`
-	Updated_at    time.Time          `json:"updated_at"`
+	Flashcards  []Flashcard
+	Created_at  time.Time `json:"created_at"`
+	Updated_at  time.Time `json:"updated_at"`
 }
