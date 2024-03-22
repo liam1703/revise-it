246654c41888f7aa90e51cc321930ad5316fec5a
Liam Bradbury <liam.bradbury98@gmail.com>
2024-03-22 17:05:03 +0000

    Populate modal

--- frontend/src/components/CreateDeckCard.vue
+++ frontend/src/components/CreateDeckCard.vue
@@ -10,12 +10,15 @@
   </div>
   <TheModal v-if="createDeckModal" >
     <div>
-      yo yo yo
+      <h3>Create new deck</h3>
+      <div class="my-4">
+        <label for="deckName" class="text-sm font-medium">Deck name:</label>
+        <input v-model="deckName" id="deckName" type="text" class="mt-2 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500">
+        <label for="deckDescription" class="text-sm font-medium">Deck description (Optional):</label> 
+        <textarea v-model="deckDescription" name="deckDescription" id="deckDescription" cols="30" rows="8" class="mt-2 p-2 text-lg w-full rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"></textarea>
+      </div>
     </div>
   </TheModal>
-
-
-
 </template>
 
 <script setup>
